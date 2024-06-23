117037cb33646aa0e5bb2613fbae37847584d637
merrymekoro <merrymekoro3m@gmail.com>
2024-06-23 17:02:21 -0500

    axios edited

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import  axios from "axios"
 
 const axiosBase = axios.create({
-    baseURL: "http://localhost:1234/api",
+    // baseURL: "http://localhost:1234/api",
+    baseURL: "https://evangadi-forum-back-end-deploy-5.onrender.com/api",
 })
 
 export default axiosBase
